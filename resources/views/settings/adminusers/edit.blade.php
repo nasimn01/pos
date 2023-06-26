98afd1277884be4cdf3871107c7c777524161528
Nasim Ahmed <nashim.idb333@gmail.com>
2023-06-26 15:22:12 +0600

    update admin users

--- resources/views/settings/adminusers/edit.blade.php
+++ resources/views/settings/adminusers/edit.blade.php
@@ -4,96 +4,95 @@
 @section('pageSubTitle',trans('Update'))
 
 @section('content')
-  <!-- // Basic multiple Column Form section start -->
-    <section id="multiple-column-form">
-        <div class="row match-height">
-            <div class="col-12">
-                <div class="card">
-                    @if(Session::has('response'))
-                        {!!Session::get('response')['message']!!}
-                    @endif
-                    <div class="card-tabs">
-                        <a class="card-tab" href="{{route(currentUser().'.admin.create')}}">Add New</a>
-                        <a class="card-tab" href="{{route(currentUser().'.admin.index')}}">List</a>
-                    </div>
-                    <div class="card-content mt-5">
-                        <div class="card-body">
-                            <form class="form" method="post" enctype="multipart/form-data" action="{{route(currentUser().'.admin.update',encryptor('encrypt',$user->id))}}">
-                                @csrf
-                                @method('PATCH')
-                                <input type="hidden" name="uptoken" value="{{encryptor('encrypt',$user->id)}}">
-                                <div class="row">
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="userName">{{__('Name')}}<span class="text-danger">*</span></label>
-                                            <input type="text" id="userName" class="form-control" value="{{ old('userName',$user->name)}}" name="userName">
-                                            @if($errors->has('userName'))
-                                                <span class="text-danger"> {{ $errors->first('userName') }}</span>
-                                            @endif
-                                        </div>
+<section id="multiple-column-form">
+    <div class="row match-height">
+        <div class="col-12">
+            <div class="card">
+                @if(Session::has('response'))
+                    {!!Session::get('response')['message']!!}
+                @endif
+                <div class="card-tabs">
+                    <a class="card-tab" href="{{route(currentUser().'.admin.create')}}">Add New</a>
+                    <a class="card-tab" href="{{route(currentUser().'.admin.index')}}">List</a>
+                </div>
+                <div class="card-content mt-5">
+                    <div class="card-body">
+                        <form class="form" method="post" enctype="multipart/form-data" action="{{route(currentUser().'.admin.update',encryptor('encrypt',$user->id))}}">
+                            @csrf
+                            @method('PATCH')
+                            <input type="hidden" name="uptoken" value="{{encryptor('encrypt',$user->id)}}">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="userName">{{__('Name')}}<span class="text-danger">*</span></label>
+                                        <input type="text" id="userName" class="form-control" value="{{ old('userName',$user->name)}}" name="userName">
+                                        @if($errors->has('userName'))
+                                            <span class="text-danger"> {{ $errors->first('userName') }}</span>
+                                        @endif
                                     </div>
+                                </div>
+                            
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="userEmail">{{__('Email')}}</label>
+                                        <input type="text" id="userEmail" class="form-control" value="{{ old('userEmail',$user->email)}}" name="userEmail">
+                                        @if($errors->has('userEmail'))
+                                            <span class="text-danger"> {{ $errors->first('userEmail') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="contactNumber">{{__('Contact Number')}}<span class="text-danger">*</span></label>
+                                        <input type="text" id="contactNumber" class="form-control" value="{{ old('contactNumber',$user->contact_no)}}" name="contactNumber">
+                                        @if($errors->has('contactNumber'))
+                                            <span class="text-danger"> {{ $errors->first('contactNumber') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
                                 
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="userEmail">{{__('Email')}}</label>
-                                            <input type="text" id="userEmail" class="form-control" value="{{ old('userEmail',$user->email)}}" name="userEmail">
-                                            @if($errors->has('userEmail'))
-                                                <span class="text-danger"> {{ $errors->first('userEmail') }}</span>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="password">{{__('Password')}}<span class="text-danger">*</span></label>
+                                        <input type="password" id="password" class="form-control" name="password">
+                                            @if($errors->has('password'))
+                                                <span class="text-danger"> {{ $errors->first('password') }}</span>
                                             @endif
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="contactNumber">{{__('Contact Number')}}<span class="text-danger">*</span></label>
-                                            <input type="text" id="contactNumber" class="form-control" value="{{ old('contactNumber',$user->contact_no)}}" name="contactNumber">
-                                            @if($errors->has('contactNumber'))
-                                                <span class="text-danger"> {{ $errors->first('contactNumber') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-                                 
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="password">{{__('Password')}}<span class="text-danger">*</span></label>
-                                            <input type="password" id="password" class="form-control" name="password">
-                                                @if($errors->has('password'))
-                                                    <span class="text-danger"> {{ $errors->first('password') }}</span>
-                                                @endif
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="image">{{__('Image')}}</label>
-                                            <input type="file" id="image" class="form-control"
-                                                placeholder="Image" name="image">
-                                                @if($errors->has('image'))
-                                                    <span class="text-danger"> {{ $errors->first('image') }}</span>
-                                                @endif
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                        <div class="form-group">
-                                            <label for="language">{{__('Language')}}</label>
-                                            <select class="form-control" name="language" id="language">
-                                                <option value="">Select Language</option>
-                                                <option value="en" {{ old('language',$user->language)=="en"?"selected":""}}> English</option>
-                                                <option value="bn" {{ old('language',$user->language)=="bn"?"selected":""}}> Bangla</option>
-                                            </select>
-                                        </div>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-12 d-flex justify-content-end">
-                                        <button type="submit" class="btn btn-info me-1 mb-1">{{__('Update')}}</button>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="image">{{__('Image')}}</label>
+                                        <input type="file" id="image" class="form-control"
+                                            placeholder="Image" name="image">
+                                            @if($errors->has('image'))
+                                                <span class="text-danger"> {{ $errors->first('image') }}</span>
+                                            @endif
                                     </div>
                                 </div>
-                            </form>
-                        </div>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="language">{{__('Language')}}</label>
+                                        <select class="form-control" name="language" id="language">
+                                            <option value="">Select Language</option>
+                                            <option value="en" {{ old('language',$user->language)=="en"?"selected":""}}> English</option>
+                                            <option value="bn" {{ old('language',$user->language)=="bn"?"selected":""}}> Bangla</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 d-flex justify-content-end">
+                                    <button type="submit" class="btn btn-info me-1 mb-1">{{__('Update')}}</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 
 @push('scripts')
